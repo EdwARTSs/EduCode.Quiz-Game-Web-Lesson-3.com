d82de49ee91de27c91a7795604928e3b8ef41de7
EdwART <122360856+EdwARTSs@users.noreply.github.com>
2023-01-12 22:42:35 +0800

    Add files via upload

---
+++ QuizGameLESSON3.js
@@ -0,0 +1,94 @@
+/*QUESTIONS FOR THE QUIZ LESSON 3*/
+
+const questions = [
+    {
+		question: 'This HTML tag the list items are marked with bullets',
+		answers: [
+			{text: 'Unordered lists', correct: true},
+			{text: 'Not ordered lists', correct: false},
+			{text: 'Bullet-lists', correct: false},
+			{text: 'list-decoration: bullet;', correct: false}
+		]
+	},
+    {
+		question: 'This HTML tag the list items are marked with numbers or letters',
+		answers: [
+			{text: 'Lists-ordered', correct: false},
+			{text: 'Ordered lists', correct: true},
+			{text: 'list-decoration: numbers;', correct: false},
+			{text: 'style: list-decoration: ordered-lists', correct: false}
+		]
+	},
+    {
+		question: 'This HTML tag is used inside ordered lists and unordered lists',
+		answers: [
+			{text: 'lists', correct: false},
+			{text: 'listing', correct: false},
+			{text: 'list', correct: true},
+			{text: 'JS.lists', correct: false}
+		]
+	},
+    {
+		question: 'This is to specify a table border in CSS',
+		answers: [
+			{text: 'css.table', correct: false},
+			{text: 'border: solid table', correct: false},
+			{text: 'Border for tables', correct: false},
+			{text: 'Table borders', correct: true}
+		]
+	},
+    {
+		question: 'The width and height of a table are defined by:',
+		answers: [
+			{text: 'width and height', correct: true},
+			{text: 'cm or inches and feet', correct: false},
+			{text: 'wd and ht', correct: false},
+			{text: 'lbs and feet tall', correct: false}
+		]
+	},
+    {
+		question: 'CSS Table Alignment: this property sets the horizontal alignment(like left, right or center)',
+		answers: [
+			{text: 'Align: horizontal;', correct: false},
+			{text: 'Horizontal Alignment', correct: true},
+			{text: 'Horizontal.Alignment', correct: false},
+			{text: 'Horizontal: Align()', correct: false}
+		]
+	},
+    {
+		question: 'CSS Table Alignment: this property sets the vertical alignment(like top, bottom or middle)',
+		answers: [
+			{text: 'Vertical: Alignthis', correct: false},
+			{text: 'Align.Vertical', correct: false},
+			{text: 'Vertical Alignment', correct: true},
+			{text: 'Align: vertical;', correct: false}
+		]
+	},
+    {
+		question: 'CSS Table Style: this control the space between the border and the content in a table, this uses the padding property',
+		answers: [
+			{text: 'Padding: Table;', correct: false},
+			{text: 'Table: Padding;', correct: false},
+			{text: 'Table.padding', correct: false},
+			{text: 'Table Padding', correct: true}
+		]
+	},
+    {
+		question: 'This HTML tag defines bold text',
+		answers: [
+			{text: '<b>', correct: true},
+			{text: '<bold>', correct: false},
+			{text: '<bolder>', correct: false},
+			{text: '<bolding>', correct: false}
+		]
+	},
+    {
+		question: 'This HTML tag defines a part of text to be in italic',
+		answers: [
+			{text: '<italic>', correct: false},
+			{text: '<i>', correct: true},
+			{text: '<tilt>', correct: false},
+			{text: '<italize>', correct: false}
+		]
+	}
+]
